78c75e0a6c04e1458c8c3e321cff20acceb18c16
1DARKlord <rfdffrs@gmail.com>
2024-05-30 13:14:22 +0300

    first commit

---
+++ prog4.bat
@@ -0,0 +1,22 @@
+echo off
+md Bernev
+cd Bernev
+md Vladimir
+md Bernev
+pause
+cd Vladimir
+echo > 05.txt
+cd..
+cd Bernev
+echo > 24012003.txt
+cd..
+cd..
+pause
+del Bernev /S /Q /F
+pause
+cd Bernev
+rd Bernev
+rd Vladimir
+cd..
+rd Bernev
+pause
---
+++ text1.bat
@@ -0,0 +1,8 @@
+rem create new catalog
+md firstcat 
+rem станкин
+cd firstcat 
+md secondcat
+cd secondcat
+md thirdcat
+rem this need for pause of programm
